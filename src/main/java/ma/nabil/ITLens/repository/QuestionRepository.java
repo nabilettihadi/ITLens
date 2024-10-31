c97538a3abcce5f0db07b9795f2bb7107bc99588
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-10-31 15:47:51 +0100

    update QuestionRepository to use Subject instead of SubChapter

--- src/main/java/ma/nabil/ITLens/repository/QuestionRepository.java
+++ src/main/java/ma/nabil/ITLens/repository/QuestionRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Page<Question> findBySubChapterId(Integer subChapterId, Pageable pageable);
-    Page<Question> findBySubChapterIdAndType(Integer subChapterId, QuestionType type, Pageable pageable);
+    Page<Question> findBySubjectId(Integer subjectId, Pageable pageable);
+    Page<Question> findBySubjectIdAndType(Integer subjectId, QuestionType type, Pageable pageable);
 }
